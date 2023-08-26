39765c30431a7c8946ccc0537456afe214740f0a
DikshantVerma <dikshantverma411@gmail.com>
2023-08-27 00:16:44 +0530

    Added MaterialUI

---
+++ src/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+import SearchIcon from '@mui/icons-material/Search';
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <img src="" alt="" />
+        <div className="header__search">
+          {/* Search Icon */}
+          <input type="text" />
+        </div>
+      </div>
+      <div className="header__right">
+        <img src="" alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
