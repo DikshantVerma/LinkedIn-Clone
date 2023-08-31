0b4553f5df3ed1840bc47d2641fbc081afc66e4c
DikshantVerma <dikshantverma411@gmail.com>
2023-08-31 23:36:25 +0530

    imported Sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,7 +1,10 @@
 import React from "react";
+import "./Sidebar.css";
 
 function Sidebar() {
-  return <div>Sidebar</div>;
+  return <div className="sidebar">
+    
+  </div>;
 }
 
 export default Sidebar;
