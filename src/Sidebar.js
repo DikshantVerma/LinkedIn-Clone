efa0fcf5de31ed2f065cf954d185ca7e0b763bc2
DikshantVerma <dikshantverma411@gmail.com>
2023-09-01 22:23:36 +0530

    Sidebar commit

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -2,9 +2,13 @@ import React from "react";
 import "./Sidebar.css";
 
 function Sidebar() {
-  return <div className="sidebar">
-    
-  </div>;
+  return (
+    <div className="sidebar">
+      <div className="sidebar__top">
+        <img src="" alt="" />
+      </div>
+    </div>
+  );
 }
 
 export default Sidebar;
