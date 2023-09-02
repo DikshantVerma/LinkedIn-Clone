a5a3b35f5177b158b1e0b00d4da3b586ddb7d902
DikshantVerma <dikshantverma411@gmail.com>
2023-09-02 23:14:41 +0530

    Added Avatar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./Sidebar.css";
+import { Avatar } from "@mui/material";
 
 function Sidebar() {
   return (
     <div className="sidebar">
       <div className="sidebar__top">
         <img src="" alt="" />
+        <Avatar />
+        <h2></h2>
+        <h4></h4>
       </div>
     </div>
   );
